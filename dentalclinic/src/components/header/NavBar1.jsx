c0397a43771864e75bc87a0531084eb69c1ec88b
Laura <Lausnclu@hotmail.com>
2023-03-14 09:36:58 +0100

    fix: Change las options anf fix my last merge

--- dentalclinic/src/components/header/NavBar1.jsx
+++ dentalclinic/src/components/header/NavBar1.jsx
@@ -9,7 +9,6 @@ import logo from "../../assets/yo.png"
 
 export const NavBar1 = () => {
   return (
-    <div className='navBarAll'>
     <Navbar sticky="top" collapseOnSelect expand="md" className='header-NavBar'>
       <Container>
         <Container>
@@ -18,21 +17,14 @@ export const NavBar1 = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav>
-          <NavDropdown title="Treatments" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Implant</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Prothesis</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Orthodontic</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Cavities</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Whitening</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link as={Link} to="/" className='size'>About us</Nav.Link>
-            <Nav.Link as={Link} to="/login">Login</Nav.Link>
-            <Nav.Link as={Link} to="/register">Register</Nav.Link>
+            <Nav.Link as={Link} to="/treatment" >Treatments</Nav.Link>
+            <Nav.Link as={Link} to="/about us" className='size'>About us</Nav.Link>
+            <Nav.Link as={Link} to="/login" >Login</Nav.Link>
+            <Nav.Link as={Link} to="/register" >Register</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    </div>
   )
 }
 
